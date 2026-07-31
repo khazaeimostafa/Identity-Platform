4f00bb0bda4d278e5264eeab7b40ae88496fca25
Mostafa <Mostafakhazaei@live.com>
2026-07-31 21:44:30 +0330

    feat(domain): add entity primitive

--- src/Identity.Domain/Primitives/Entity.cs
+++ src/Identity.Domain/Primitives/Entity.cs
@@ -1,6 +1,16 @@
 namespace Identity.Domain.Primitives;
 
-public class Entity<TId> where TId :  notnull
+public class Entity<TId> : IEquatable<Entity<TId>> where TId : notnull
 {
+
+    public TId Id { get; }
     
+    
+    public bool Equals(Entity<TId>? other)
+    {
+        if (other is null)   
+         return false;
+         if (ReferenceEquals(this,other)) return true;
+         return Id.Equals(other.Id);
+    }
 }
